64618c366d18a8a105ba07b665cf8817cff5d8cc
Ricky Wijaya <ricky.w@weefer.co.id>
2022-03-17 22:30:30 +0700

    Init Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const data = () => {
+    return {
+        name: "Ricky Wijaya",
+        weefer: "https://www.weefer.co.id/",
+        instagram: "https://www.instagram.com/rrwwii_/",
+        linkedIn: "https://www.linkedin.com/in/rickyywijayaa",
+        github: "https://github.com/rickywijayaa",
+    }
+}
